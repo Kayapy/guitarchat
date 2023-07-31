150b20bc62adbba8a1bfb0cf0a9e6a1a136861c8
Kayapy <kayapybruno@gmail.com>
2023-07-31 12:46:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import streamlit as st
 import openai
 
-# Configure a API key do OpenAI
-openai.api_key = "sua_api_key_aqui"
+# Configure a API key do OpenAI (NÃO inclua a chave diretamente no código!)
+# openai.api_key = "sua_api_key_aqui"
 
 st.title("Guitar Chat")
 st.subheader("Guitar Riff Idea Generator")
 
 Guitar_Riff_Idea_Generator = st.text_input("Type Here What You Want To Create on the Guitar:")
 
-# Verifique se há entrada do usuário e faça a chamada à API do GPT-3.5
+# Chame a API do GPT-3.5 para gerar a resposta
 if Guitar_Riff_Idea_Generator:
-    st.subheader("Você digitou: ")
-    st.write(Guitar_Riff_Idea_Generator)
-
-    # Chame a API do GPT-3.5 para gerar a resposta
     response = openai.Completion.create(
-        engine="text-davinci-002",  # Especifique o motor do GPT-3.5 (pode variar dependendo da disponibilidade)
+        engine="text-davinci-002",
         prompt=Guitar_Riff_Idea_Generator,
-        max_tokens=100  # Limite o tamanho da resposta, se necessário
+        max_tokens=100
     )
 
-    # Exiba a resposta gerada pelo modelo
-    st.subheader("Resposta do GPT-3.5:")
-    st.write(response.choices[0].text)
+    # Verifique se a resposta gerada pelo modelo é válida
+    if response.choices and response.choices[0].text.strip():
+        # Exiba a resposta gerada pelo modelo
+        st.subheader("Resposta do GPT-3.5:")
+        st.write(response.choices[0].text)
+    else:
+        st.subheader("O modelo não retornou uma resposta válida.")
+
 
