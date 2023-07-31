f363f342cc4e1090a415273cc02cff71002ba966
Kayapy <kayapybruno@gmail.com>
2023-07-31 04:59:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Guitar Chat")
+st.subheader("Guitar Riff Idea Generator")
+
+Guitar_Riff_Idea_Generator = st.text_input("Type Here What You Want To Create on the Guitar:")
+
+# Resto do código do seu aplicativo Streamlit
+if Guitar_Riff_Idea_Generator:
+    st.subheader("Você digitou: ")
+    st.write(Guitar_Riff_Idea_Generator)
