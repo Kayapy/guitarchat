bb65a90153b61025f6f5b13714e32d4f4146e835
Kayapy <kayapybruno@gmail.com>
2023-07-31 14:52:44 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+st.title("Guitar Chat")
+st.subheader("Guitar Riff Idea Generator")
+
+Guitar_Riff_Idea_Generator = st.text_input("Type Here What You Want To Create on the Guitar:")
+
+# Chame a API do GPT-3.5 para gerar a resposta
+if Guitar_Riff_Idea_Generator:
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=Guitar_Riff_Idea_Generator,
+        max_tokens=100
+    )
+
+    # Verifique se a resposta gerada pelo modelo é válida
+    if response.choices and response.choices[0].text.strip():
+        # Exiba a resposta gerada pelo modelo
+        st.subheader("Resposta do GPT-3.5:")
+        st.write(response.choices[0].text)
+    else:
+        st.subheader("O modelo não retornou uma resposta válida.")
