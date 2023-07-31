b0bfbb61a387b42b20096b69487b20b2b07ed674
Kayapy <kayapybruno@gmail.com>
2023-07-31 12:35:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-import openai
 import streamlit as st
-import toml
+import openai
 
-# Carregar as informações sensíveis do arquivo secrets.toml
-secrets = toml.load("secrets.toml")
+# Configure a API key do OpenAI
+openai.api_key = "sua_api_key_aqui"
 
-# Configurar a API key da OpenAI
-openai.api_key = secrets["openai_credentials"]["api_key"]
-
-# Resto do código do seu aplicativo Streamlit
 st.title("Guitar Chat")
 st.subheader("Guitar Riff Idea Generator")
 
 Guitar_Riff_Idea_Generator = st.text_input("Type Here What You Want To Create on the Guitar:")
 
+# Verifique se há entrada do usuário e faça a chamada à API do GPT-3.5
 if Guitar_Riff_Idea_Generator:
-    resposta = openai.Completion.create(
-        model="text-davinci-003",
+    st.subheader("Você digitou: ")
+    st.write(Guitar_Riff_Idea_Generator)
+
+    # Chame a API do GPT-3.5 para gerar a resposta
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # Especifique o motor do GPT-3.5 (pode variar dependendo da disponibilidade)
         prompt=Guitar_Riff_Idea_Generator,
-        max_tokens=1000,
-        n=1
+        max_tokens=100  # Limite o tamanho da resposta, se necessário
     )
-    st.subheader("RESPOSTA")
-    st.write(resposta.choices[0].text)
+
+    # Exiba a resposta gerada pelo modelo
+    st.subheader("Resposta do GPT-3.5:")
+    st.write(response.choices[0].text)
 
